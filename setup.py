a68702eeba4bbf99fcc5c9359465d5d5bcb2b89d
Josh Newman <josh@teambanjo.com>
2017-11-03 23:50:45 -0700

    (Feat) Base notebook to load data

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='lo-analysis',
+      version='0.0.01',
+      description='',
+      author='',
+      author_email='',
+      packages=find_packages(exclude=('tests')),
+      zip_safe=False)
